82edd821915d1f78c1327c599a1446c958a3229e
Anthony Li <me@anli.dev>
2025-12-01 00:05:52 -0500

    It's that time again

---
+++ 1/1a.py
@@ -0,0 +1,36 @@
+import sys
+import re
+from collections import *
+from itertools import *
+from heapq import *
+from dataclasses import *
+import functools
+import math
+
+def count(it):
+    return sum(1 for _ in it)
+
+inp = sys.stdin.read()
+lines = inp.split("\n")
+m = len(lines)
+n = len(lines[0])
+
+grid = defaultdict((lambda: defaultdict(lambda: "!")), enumerate(defaultdict((lambda: "!"), enumerate(line)) for line in lines))
+dirs = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+diag = [(-1, -1), (1, 1), (-1, 1), (1, -1)]
+# dirs = dirs + diag
+
+result = 0
+pos = 50
+
+for line in lines:
+    amount = int(line[1:])
+    if line[0] == "L":
+        pos -= amount
+    else:
+        pos += amount
+    pos = (pos + 100) % 100
+    if pos == 0:
+        result += 1
+
+print(result)
---
+++ scripts/aoc.zsh
@@ -0,0 +1,70 @@
+export AOC_ROOT="$(dirname "$(dirname "$(realpath "$0")")")"
+export PYTHONPATH=$AOC_ROOT:$PYTHONPATH
+
+day() {
+    python $AOC_ROOT/scripts/day.py
+}
+
+setup() {
+    if [ ! -f $AOC_ROOT/.aocsession ]; then
+        echo "No .aocsession file found. Please create one before continuing."
+        return
+    fi
+
+    DAY="$(day)"
+    YEAR="$(python $AOC_ROOT/scripts/year.py)"
+    echo "Setting up for day $DAY of $YEAR..."
+    mkdir $AOC_ROOT/$DAY
+    cp $AOC_ROOT/template.py $AOC_ROOT/$DAY/$(echo $DAY)a.py
+    touch $AOC_ROOT/$DAY/$(echo $DAY)b.py
+    touch $AOC_ROOT/$DAY/s.txt
+    code $AOC_ROOT --goto $DAY/$(echo $DAY)a.py
+    if type xdg-open > /dev/null; then
+        xdg-open https://adventofcode.com/$YEAR/day/$DAY
+    else
+        open https://adventofcode.com/$YEAR/day/$DAY
+    fi
+    curl -A "https://github.com/anli5005/advent-of-code-$YEAR by me@anli.dev" https://adventofcode.com/$YEAR/day/$DAY/input --cookie "session=$(cat $AOC_ROOT/.aocsession)" > $AOC_ROOT/$DAY/$(echo $DAY).txt
+    truncate -s -1 $AOC_ROOT/$DAY/$(echo $DAY).txt
+}
+
+setuptimer() {
+    if [ ! -f $AOC_ROOT/.aocsession ]; then
+        echo "No .aocsession file found. Please create one before continuing."
+        return
+    fi
+    
+    python $AOC_ROOT/scripts/waitformidnight.py && setup
+}
+
+a() {
+    DAY="$(day)"
+    python $AOC_ROOT/$DAY/$(echo $DAY)a.py < $AOC_ROOT/$DAY/$(echo $DAY).txt
+}
+
+as() {
+    DAY="$(day)"
+    python $AOC_ROOT/$DAY/$(echo $DAY)a.py < $AOC_ROOT/$DAY/s.txt
+}
+
+b() {
+    DAY="$(day)"
+    python $AOC_ROOT/$DAY/$(echo $DAY)b.py < $AOC_ROOT/$DAY/$(echo $DAY).txt
+}
+
+bs() {
+    DAY="$(day)"
+    python $AOC_ROOT/$DAY/$(echo $DAY)b.py < $AOC_ROOT/$DAY/s.txt
+}
+
+echo "Advent of Code 2025 environment loaded!"
+echo "setup - create new day and download input"
+echo "setuptimer - create new day and download input at midnight"
+echo "a - run part a"
+echo "as - run part a with sample input"
+echo "b - run part b"
+echo "bs - run part b with sample input"
+
+if [ ! -f $AOC_ROOT/.aocsession ]; then
+    echo "No .aocsession file found. Please create one before running setup."
+fi
