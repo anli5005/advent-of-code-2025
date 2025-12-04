01aa15917c7014b62b635c2a31408eca8d49ffb4
Anthony Li <me@anli.dev>
2025-12-04 00:03:47 -0500

    Day 4

---
+++ 04/04b.py
@@ -0,0 +1,40 @@
+import sys
+import re
+from collections import *
+from itertools import *
+from heapq import *
+from dataclasses import *
+import functools
+import math
+
+inp = sys.stdin.read()
+lines = inp.split("\n")
+m = len(lines)
+n = len(lines[0])
+
+grid = defaultdict((lambda: defaultdict(lambda: "!")), enumerate(defaultdict((lambda: "!"), enumerate(line)) for line in lines))
+dirs = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+diag = [(-1, -1), (1, 1), (-1, 1), (1, -1)]
+dirs = dirs + diag
+
+result = 0
+
+while True:
+    removal_queue = []
+    for i in range(m):
+        for j in range(n):
+            if grid[i][j] != "@":
+                continue
+            rolls = 0
+            for di, dj in dirs:
+                if grid[i + di][j + dj] == "@":
+                    rolls += 1
+            if rolls < 4:
+                removal_queue.append((i, j))
+    if len(removal_queue) == 0:
+        break
+    result += len(removal_queue)
+    for i, j in removal_queue:
+        grid[i][j] = "."
+
+print(result)
